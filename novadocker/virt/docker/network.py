d4ec2cefdf92ad513f771a57e093638a579cf221
intlabs <petebirley@gmail.com>
2016-09-15 03:20:43 +0100

    disable metadata

--- novadocker/virt/docker/network.py
+++ novadocker/virt/docker/network.py
@@ -60,7 +60,7 @@ def find_gateway(instance, network_info):
 
 def find_dhcp(instance, network_info):
     for subnet in network_info['subnets']:
-        return subnet['meta']['dhcp_server']
+        return subnet['gateway']['address']
     raise exception.InstanceDeployFailure(_('Cannot find dhcp'),
                                           instance_id=instance['uuid'])
 
